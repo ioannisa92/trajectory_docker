53da9f4c1d5c9439a81cba85715f1e69d782f219
ioannisa92 <ianastop@ucsc.edu>
2019-02-11 21:40:24 +0000

    renaming

---
+++ SLICER/src/SLICER_example.R
@@ -0,0 +1,23 @@
+library(SLICER)
+library(lle)
+
+print(dim(traj))
+
+genes = select_genes(traj)
+k = select_k(traj[,genes], kmin=5)
+traj_lle = lle(traj[,genes], m=2, k)$Y
+
+traj_graph = conn_knn_graph(traj_lle,5)
+ends = find_extreme_cells(traj_graph, traj_lle)
+
+start = 1
+cells_ordered = cell_order(traj_graph, start)
+branches = assign_branches(traj_graph,start)
+
+compute_geodesic_entropy(traj_graph, start)
+
+graph_gene(traj, traj_lle, seq(1, 100), 10, cell_symbols = 16,
+  title = "Gene Expression")
+
+graph_process_distance(traj_graph, traj_lle, start, cell_symbols = 16)
+
